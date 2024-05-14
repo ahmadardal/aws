e7d0e5ced2d623b5549278541ed4f68e2e6f6479
ahmadardal <ahmadardal@live.se>
2024-05-14 10:22:39 +0200

    Simplify condition

--- src/aws.cr
+++ src/aws.cr
@@ -4,11 +4,7 @@ module AWS
   class_property access_key_id : String = ENV["AWS_ACCESS_KEY_ID"]? || ""
   class_property secret_access_key : String = ENV["AWS_SECRET_ACCESS_KEY"]? || ""
   class_property region : String = ENV["AWS_REGION"]? || ""
-  class_property use_tls : Bool = case ENV["AWS_USE_TLS"]?
-  when "true"  then true
-  when "false" then false
-  else              true
-  end
+  class_property use_tls : Bool = ENV["AWS_USE_TLS"] == "false" ? false : true
 
   class Exception < ::Exception
   end
