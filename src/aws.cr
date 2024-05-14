c1400017ae56d23d1c84af0d5bc94dd4e8642c97
ahmadardal <ahmadardal@live.se>
2024-05-14 08:38:39 +0200

    Optional TLS

--- src/aws.cr
+++ src/aws.cr
@@ -4,9 +4,15 @@ module AWS
   class_property access_key_id : String = ENV["AWS_ACCESS_KEY_ID"]? || ""
   class_property secret_access_key : String = ENV["AWS_SECRET_ACCESS_KEY"]? || ""
   class_property region : String = ENV["AWS_REGION"]? || ""
+  class_property use_tls : Bool = case ENV["AWS_USE_TLS"]?
+  when "true"  then true
+  when "false" then false
+  else              true
+  end
 
   class Exception < ::Exception
   end
+
   class InvalidCredentials < Exception
   end
 end
